1b2441ea9733be515b75b8113280aab67a6e9485
NEKOTENI <riverfield.tk@gmail.com>
2019-12-02 14:42:42 +0900

    first challenge

---
+++ q01_palindrome.rb
@@ -0,0 +1,15 @@
+# 11以上の数から探索開始
+num = 11
+
+while true
+  if
+    num.to_s == num.to_s.reverse &&
+    num.to_s(8) == num.to_s(8).reverse &&
+    num.to_s(2) == num.to_s(2).reverse
+    
+    puts num
+    break
+  end
+# 奇数だけを探すため、numを2ずつ増やす
+  num += 2
+end
