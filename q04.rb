a276e2fee6b8e62b438c37da2911c5f9673fae30
NEKOTENI <riverfield.tk@gmail.com>
2020-01-14 10:12:51 +0900

    思い出し。初見とはやっぱり違うね。よい。

--- q04.rb
+++ q04.rb
@@ -1,31 +1,45 @@
-# def cutbar(m, n, current)
-#   if current >= n then
-#     0
-#   elsif current < m then
-#     1 + cutbar(m, n, current * 2)
-#   else
-#     1 + cutbar(m, n, current + m)
-#   end
-# end
-# 
-# puts cutbar(3, 20, 1)
-# puts cutbar(5, 100, 1)
+#長さnセンチの棒
+#1センチ単位に切り分ける
+#m人の人
+#最短何回で切り分けができるか
+
+def cutbar(man, n, current)
+  #現在の棒の数が現在の棒の長さ以上だったら
+  if current >= n then
+    0 #切り終えた
+  #現在棒の数が人の数よりも少なかったら
+  elsif current < man then 
+    1 + cutbar(man, n, current * 2)
+  #上記以外
+  else
+    1 + cutbar(man, n, current + man)
+  end
+end
+
+puts cutbar(3, 20, 1)
+puts cutbar(5, 100, 1)
 
 
-def cutbar2(m, n)
-  count = 0 # 何回切ったかのカウント
-  current = 1 # 棒の数。最初は1本。
+def cutbar2(man, n)
+  count = 0
+  current = 1 #currentは現在の長さ
+
   while n > current do
-    current += current < m ? current : m # 三項演算子きたー！
+    #三項演算子。?の左の条件を満たす場合は、currentを実行、満たさない場合はmanを実行する。
+    current += current < man ? current : man
     count = count + 1
   end
+
   puts(count)
 end
 
-puts cutbar2(3, 20)
-puts cutbar2(5, 100)
+cutbar2(3, 20)
+cutbar2(5, 100)
 
-# day1
-# 本の写経。今回は2つ答えがある。2つめの方がわかりやすくて好み。
-# 再帰呼び出しすることがイテレーションになるのが感覚的にわからない・・・。
-# 三項演算子に慣れないとなぁ。
+
+
+# day3
+# 久しぶりなので本の写経。
+# やっぱりcutbar2の方が受け入れやすい（というか理解しやすい）。
+# cutbarは再帰呼び出しを抜きにしても、コードの内容の理解が難しい。
+# 繰り返しやろう。
